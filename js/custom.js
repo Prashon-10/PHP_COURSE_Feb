fa081960a9c09263da59b410b9172b156a240bb4
prashon <prashon109939@gmail.com>
2024-02-04 09:30:19 +0545

    new commit

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+function getHTTP() {
+  // let http = new XMLHttpRequest();
+  // http.onreadystatechange = function(){
+  //     if(http.readyState == 4 && http.status == 200){
+  //         console.log(JSON.parse(http.responseText))
+  //     }
+  // }
+
+  // http.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+  // http.send();
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+getHTTP();
