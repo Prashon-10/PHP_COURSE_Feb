2a87b29d61d6cb7db76959ab98f91982cf81187e
prashon <prashon109939@gmail.com>
2024-02-04 09:37:54 +0545

    new commitAJAX and others

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,34 @@
-function getHTTP() {
-  // let http = new XMLHttpRequest();
-  // http.onreadystatechange = function(){
-  //     if(http.readyState == 4 && http.status == 200){
-  //         console.log(JSON.parse(http.responseText))
-  //     }
-  // }
+// function getHTTP() {
+//   // let http = new XMLHttpRequest();
+//   // http.onreadystatechange = function(){
+//   //     if(http.readyState == 4 && http.status == 200){
+//   //         console.log(JSON.parse(http.responseText))
+//   //     }
+//   // }
 
-  // http.open("GET", "https://jsonplaceholder.typicode.com/users", true);
-  // http.send();
+//   // http.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+//   // http.send();
 
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => {
-      console.log(err);
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+
+// getHTTP();
+
+$(document).ready(function(){
+    $('#hide').click(function () { 
+        $('p').hide();
     });
-}
 
-getHTTP();
+    $('#show').click(function () { 
+        $('p').show();
+    });
+});
